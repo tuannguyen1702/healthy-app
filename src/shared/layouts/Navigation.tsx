d3cc6ea5dc795a11e7fb5a21d3b560e98de37d1c
Tuan Nguyen <tuan.nguyen@pops.vn>
2023-07-10 13:48:53 +0700

    add dropdown menu

--- src/shared/layouts/Navigation.tsx
+++ src/shared/layouts/Navigation.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const Header = () => {
     const router = useRouter();
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+    const openMenuHandle = () => {
+        setIsOpenMenu((prevValue) => !prevValue);
+    }
+
+    const goToPage = (url: string) => {
+        setIsOpenMenu(false);
+        router.push(url);
+    }
     return (
         <nav className="text-white font-light flex justify-end gap-8">
             <Link href={'/my-record'} className="flex items-center space-x-2">
@@ -51,14 +61,30 @@ const Header = () => {
 
                 <span>お知らせ</span>
             </a>
-            <a href="#" className="flex items-center space-x-2">
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect opacity="0.01" width="32" height="32" fill="#FF963C" />
-                    <path d="M3 8H29" stroke="#FF963C" stroke-width="3" />
-                    <path d="M3 16H29" stroke="#FF963C" stroke-width="3" />
-                    <path d="M3 24H29" stroke="#FF963C" stroke-width="3" />
-                </svg>
-            </a>
+            <div className="relative flex items-center space-x-2">
+                <button onClick={openMenuHandle} className="focus:outline-none">
+                    {!isOpenMenu ?
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect opacity="0.01" width="32" height="32" fill="#FF963C" />
+                            <path d="M3 8H29" stroke="#FF963C" stroke-width="3" />
+                            <path d="M3 16H29" stroke="#FF963C" stroke-width="3" />
+                            <path d="M3 24H29" stroke="#FF963C" stroke-width="3" />
+                        </svg>
+                        : <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                            <rect opacity="0.01" width="32" height="32" fill="#FF963C" />
+                            <path d="M7 7L26 26" stroke="#FF963C" stroke-width="3" />
+                            <path d="M7 26L26 7" stroke="#FF963C" stroke-width="3" />
+                        </svg>}
+                </button>
+                <div className={'absolute top-full right-0 mt-4 text-white drop-down-menu ' + (!isOpenMenu ? 'hidden':'')}>
+                    <a onClick={() => goToPage('')}>自分の記録</a>
+                    <a onClick={() => goToPage('')}>体重グラフ</a>
+                    <a onClick={() => goToPage('')}>目標</a>
+                    <a onClick={() => goToPage('')}>選択中のコース</a>
+                    <a onClick={() =>goToPage('/column')}>コラム一覧</a>
+                    <a onClick={() => goToPage('')}>設定</a>
+                </div>
+            </div>
         </nav>
     );
 };
